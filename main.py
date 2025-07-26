2dff0dcbbc04ae812c97d44e7d3b96cd590049ef
gauravbansal74 <gauravbansal74@gmail.com>
2025-07-26 18:06:17 +0800

    updated

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    x_train, x_test, y_train, y_test = BaseLineModel.load_dataset()
+    x_train, y_train, x_test, y_test = BaseLineModel.load_dataset()
     print('Train: X=%s, y=%s' % (x_train.shape, y_train.shape))
-    print('Test: X=%s, y=%s' %( x_test.shape, y_test.shape))
+    print('Test: X=%s, y=%s' % (x_test.shape, y_test.shape))
     BaseLineModel.plot_dataset(x_train, y_train, x_test, y_test)
     x_train, x_test = BaseLineModel.prepare_dataset(x_train, x_test)
     model = BaseLineModel.define_model()
     model.fit(x_train, y_train, epochs=10, batch_size=32, verbose=0)
-    model.save('final_model.h5')
+    model.save('final_model.keras')
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
